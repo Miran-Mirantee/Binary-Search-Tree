000330cd099ec4761b1c030078728aa4456c3365
Miran-Mirantee <killerm11805@hotmail.com>
2023-02-15 23:07:31 +0700

    Implement remove duplicate element and quick sort function

--- bst.js
+++ bst.js
@@ -11,3 +11,47 @@ class Tree {
     this.root = null;
   }
 }
+
+const removeDup = (arr) => {
+  const unique = [...new Set(arr)];
+  return unique;
+};
+
+const swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+const partition = (arr, low, high) => {
+  let pivot = arr[high];
+  let i = low - 1;
+
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      swap(arr, i, j);
+    }
+  }
+  swap(arr, i + 1, high);
+  return i + 1;
+};
+
+const quickSort = (arr, low, high) => {
+  if (low < high) {
+    let pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+};
+
+const buildTree = (arr) => {
+  const unique = removeDup(arr);
+  let n = unique.length;
+  quickSort(unique, 0, n - 1);
+  console.log(unique);
+};
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+buildTree(arr);
